8793d9fa6f1667e9afdb35d0b44874ad777cb521
Franco Muñoz <famunoz13@uc.cl>
2017-02-02 14:13:00 -0300

    Add material-ui

--- src/App.js
+++ src/App.js
@@ -1,20 +1,33 @@
 import React, { Component } from 'react';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import RaisedButton from 'material-ui/RaisedButton';
+import TextField from 'material-ui/TextField';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  handleClick(e) {
+    e.preventDefault();
+    console.log("A button!");
+  }
+
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Practiweb</h2>
+      <MuiThemeProvider>
+        <div className="App">
+          <div className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h2>Practiweb</h2>
+          </div>
+          <h1>Hello world!</h1>
+          <p className="App-intro">
+            To get started, edit <code>src/App.js</code> and save to reload.
+          </p>
+          <RaisedButton onClick={this.handleClick}>A button!</RaisedButton>
+          <br />
+          <TextField hintText="An input"/>
         </div>
-        <h1>Hello world!</h1>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      </MuiThemeProvider>
     );
   }
 }
